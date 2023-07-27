07cb91073dd0667deb3378f4bb1fa8b27d7ac0e5
MENGSRUN <85465677+MENGSRUN@users.noreply.github.com>
2023-07-27 16:13:43 +0700

    First Commit

---
+++ src/order/Offc.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="card mb-3" style="max-width: auto;" v-for="item in Order1" :key="item.No">
+    <div class="row g-0">
+
+
+      <cart/>
+      <div class="col-md-4">
+
+        <img :src="item.Images" class="img-fluid rounded-start" alt="...">
+
+      </div>
+
+      <div class="col-md-8">
+        <div class="card-body">
+          <tr>
+
+            <td>{{ item.Brands }}</td>
+            <td>{{ item.Models }}</td>
+            <td>{{ item.Prices }}$</td>
+          </tr>
+          <button type="button" class="btn btn-danger btn-sm" style="border-radius: 100%;"
+            @click="deleteProduct(item.No)"><i class="fa-solid fa-dumpster"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+td {
+  padding-right: 4px;
+}
+</style>
+
+<script>
+import axios from "axios";
+import cart from "@/components/cart.vue"
+export default {
+  components: {
+    cart,
+  },
+  
+  name: "Order",
+  
+  data() {
+    return {
+      Order1: [],
+      Order2: []
+
+    };
+  },
+
+  created() {
+    this.getProducts();
+    this.getSum();
+
+  },
+
+  methods: {
+
+    // Get All Products
+    async getProducts() {
+      try {
+        const response = await axios.get("http://localhost:5000/order");
+        this.Order1 = response.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+
+    async deleteProduct(No) {
+      try {
+        await axios.delete(`http://localhost:5000/order/${No}`);
+        this.getProducts();
+        location.reload();
+
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async getSum() {
+      try {
+        const response = await axios.get("http://localhost:5000/order/sum");
+        this.Order2 = response.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async deleteProductall() {
+      try {
+        await axios.delete(`http://localhost:5000/order`);
+        this.getProducts();
+        location.reload();
+
+      } catch (err) {
+        console.log(err);
+      }
+    },
+
+  },
+};
+</script>
+<style scoped>
+.navbar {
+  height: 12vh;
+  width: 50vh;
+  margin-left: 158.5vh;
+  padding-right: 8vh;
+  
+}
+
+.reaksa {
+  display: flex;
+  align-items: center;
+}
+.meng{
+  display: flex;
+  margin-left: 36px;
+
+  /* align-items: center; */
+}
+.srun{
+  margin-left: 3vh;
+}
+
+</style>
